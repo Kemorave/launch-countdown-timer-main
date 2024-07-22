fa40d61738f5f7f5fe979b56bc4991d6d1d48f91
Kemorave <Kemorave@gmail.com>
2024-07-22 14:16:00 +0300

    up

--- scripts/flipped_card.js
+++ scripts/flipped_card.js
@@ -1,11 +1,19 @@
 function startFlipper() {
-  flipDown("flip-1");
+  flipDown("flip-1", "tx", "tx1", "123456788"); 
 }
-function flipDown(id) {
+function flipDown(id, txId, tx2Id, val) {
   let secFlip = document.getElementById(id);
-  for (let index = 0; index < 90; index++) {
+  let tx = document.getElementById(txId);
+  let tx2 = document.getElementById(tx2Id);
+  for (let index = 0; index < 180; index++) {
+    if (index == 90) {
+      tx.innerText = val;
+      tx2.innerText = val;
+    }
     setTimeout(() => {
-      secFlip.style.transform = `rotateX(${index}deg)`;
+      secFlip.style.transform = `rotateX(${index}deg) rotateY(${
+        index >= 90 ? 90 : 0
+      }deg)`;
     }, 200 + index * 10);
   }
 }
